64ff738725eb554936da41c58978dca7f54edf7e
ocaocuoci <hernandar0606@gmail.com>
2024-11-15 12:01:19 +0700

    percobaan 1 nomer 2

--- TerimaKasih_12.java
+++ TerimaKasih_12.java
@@ -3,12 +3,7 @@ public class TerimaKasih_12{
         public static void UcapanTerimaKasih(){
             System.out.println("Thank you for being the best teacher in the world. \n" + "You inspired in me a love for learning and made me feel like i could ask you anything.");
         }
-        public static void main(String[] args){
-            UcapanTerimaKasih();
-            String ucapan = "Terima kasih Pak... Bu... Semoga sehat selalu";
-            UcapanTambahan(ucapan);
-        }
-        public static void UcapanTambahan(String ucapan){
-            System.out.println(ucapan);
+        public static void main(String []args){
+           System.out.println("Thank you for being the best teacher in the world. \n" + "You inspired in me a love for learning and made me feel like i could ask you anything.");
         }
 }
