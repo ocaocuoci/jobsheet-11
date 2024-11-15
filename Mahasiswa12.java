cc54ef0dbd3884d3720c3d38fd584fbac90f02f1
ocaocuoci <hernandar0606@gmail.com>
2024-11-15 12:52:59 +0700

    Tugas 2 modifikasi input jumlah dan nilai tugas

--- Mahasiswa12.java
+++ Mahasiswa12.java
@@ -1,38 +1,58 @@
 import java.util.Scanner;
+
 public class Mahasiswa12 {
-
-    static String[] namaMahasiswa = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
-    static int[][] nilaiMahasiswa = new int[5][7]; 
-
+    static String[] namaMahasiswa;
+    static int[][] nilaiMahasiswa;
+    static Scanner input = new Scanner(System.in);
+    
+    public static void initializeArrays() {
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = input.nextInt();
+        input.nextLine();
+        
+        System.out.print("Masukkan jumlah tugas/minggu: ");
+        int jumlahTugas = input.nextInt();
+        input.nextLine(); 
+        
+        namaMahasiswa = new String[jumlahMahasiswa];
+        nilaiMahasiswa = new int[jumlahMahasiswa][jumlahTugas];
+        
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.print("Masukkan nama mahasiswa ke-" + (i + 1) + ": ");
+            namaMahasiswa[i] = input.nextLine();
+        }
+    }
+    
     public static void inputNilai() {
-        Scanner scanner = new Scanner(System.in);
-
         for (int i = 0; i < namaMahasiswa.length; i++) {
             System.out.println("Masukkan nilai untuk " + namaMahasiswa[i] + ":");
-
-            for (int j = 0; j < nilaiMahasiswa[0].length; j++) { //7
+            
+            for (int j = 0; j < nilaiMahasiswa[0].length; j++) {
                 System.out.print("Minggu ke-" + (j + 1) + ": ");
-                nilaiMahasiswa[i][j] = scanner.nextInt();
+                nilaiMahasiswa[i][j] = input.nextInt();
             }
         }
     }
-
+    
     public static void tampilkanNilai() {
         System.out.println("\nData Nilai Mahasiswa:");
-
+        
         for (int i = 0; i < namaMahasiswa.length; i++) {
             System.out.print(namaMahasiswa[i] + " : ");
-
+            
             for (int j = 0; j < nilaiMahasiswa[0].length; j++) {
-                System.out.print(nilaiMahasiswa[i][j] + ", ");
+                System.out.print(nilaiMahasiswa[i][j]);
+                if (j < nilaiMahasiswa[0].length - 1) {
+                    System.out.print(", ");
+                }
             }
             System.out.println();
         }
     }
-
+    
     public static void nilaiTertinggiPerMinggu() {
         System.out.println("\nNilai tertinggi di setiap minggu:");
-        for ( int minggu = 0; minggu < 7; minggu++) {
+        for (int minggu = 0; minggu < nilaiMahasiswa[0].length; minggu++) {
             int nilaiTertinggi = 0;
             for (int mahasiswa = 0; mahasiswa < namaMahasiswa.length; mahasiswa++) {
                 if (nilaiMahasiswa[mahasiswa][minggu] > nilaiTertinggi) {
@@ -42,14 +62,14 @@ public class Mahasiswa12 {
             System.out.println("Minggu ke-" + (minggu + 1) + ": " + nilaiTertinggi);
         }
     }
-
+    
     public static void mahasiswaNilaiTertinggi() {
         int nilaiTertinggi = 0;
         String namaMahasiswaTertinggi = "";
         int mingguTertinggi = 0;
-
+        
         for (int i = 0; i < namaMahasiswa.length; i++) {
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < nilaiMahasiswa[0].length; j++) {
                 if (nilaiMahasiswa[i][j] > nilaiTertinggi) {
                     nilaiTertinggi = nilaiMahasiswa[i][j];
                     namaMahasiswaTertinggi = namaMahasiswa[i];
@@ -57,20 +77,18 @@ public class Mahasiswa12 {
                 }
             }
         }
-
+        
         System.out.println("\nMahasiswa dengan nilai tertinggi:");
         System.out.println("Nama: " + namaMahasiswaTertinggi);
         System.out.println("Nilai Tertinggi: " + nilaiTertinggi);
         System.out.println("Minggu ke-" + mingguTertinggi);
     }
-
+    
     public static void main(String[] args) {
+        initializeArrays();
         inputNilai();
-
         tampilkanNilai();
-
         nilaiTertinggiPerMinggu();
-
         mahasiswaNilaiTertinggi();
     }
 }
