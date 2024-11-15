93fa0c597fcff90ea8e9bc903267dd56daf91bef
ocaocuoci <hernandar0606@gmail.com>
2024-11-15 12:39:05 +0700

    percobaan 4 modifikasi program

--- UcapanTerimaKasih_12.java
+++ UcapanTerimaKasih_12.java
@@ -12,6 +12,12 @@ public class UcapanTerimaKasih_12{
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
         System.out.println("Thank you for being the best teacher in the world. \n" + "You inspired in me a love for learning and made me feel like i could ask you anything.");
+
+        String ucapanTambahan = "I appreciate all your hard work and dedication!";
+        ucapanTambahan(ucapanTambahan);
+    }
+    public static void ucapanTambahan(String ucapan){
+        System.out.println(ucapan);
     }
     public static void main(String[] args){
         UcapanTerimaKasih();
